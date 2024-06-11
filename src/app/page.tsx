9bf44129ae90de82e0188de378fdfa2d62ebff97
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-06-11 13:53:34 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {OurTeam} from "@/app/[locale]/components/our-team/OurTeam";
-import styles from './page.module.scss'
+import styles from '@/app/[locale]/page.module.scss'
 import { HelpOptions } from '@/app/[locale]/pages/main-page/second-part/help-options/HelpOptions'
 import { OurPartners } from '@/app/[locale]/pages/main-page/second-part/our-partners/OurPartners'
 import { GetHelp } from '@/app/[locale]/pages/main-page/second-part/get-help/GetHelp'
 import { DonateHelp } from '@/app/[locale]/pages/main-page/second-part/donate-help/DonateHelp'
 import { Footer } from '@/app/[locale]/components/footer/Footer'
-import stylesMain from './pages/main-page/first-part/styles/main.module.scss'
+import stylesMain from '@/app/[locale]/pages/main-page/first-part/styles/main.module.scss'
 import Header from '@/app/[locale]/components/header/Header'
 import Firstblock from '@/app/[locale]/pages/main-page/first-part/pages/firtsBlock'
 import News from '@/app/[locale]/pages/main-page/first-part/pages/news'
